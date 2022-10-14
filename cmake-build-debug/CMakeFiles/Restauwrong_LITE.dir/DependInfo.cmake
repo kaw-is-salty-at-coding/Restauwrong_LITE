5e71592861188b675d5e8b01130c6bc6c8a7b24d
kaw-is-salty-at-coding <kawin1311@gmail.com>
2022-10-14 16:20:43 +0700

    15.49 14/10/22

---
+++ cmake-build-debug/CMakeFiles/Restauwrong_LITE.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kawin/CLionProjects/Restauwrong_LITE2/main.cpp" "CMakeFiles/Restauwrong_LITE.dir/main.cpp.obj" "gcc" "CMakeFiles/Restauwrong_LITE.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Restauwrong_LITE.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Restauwrong_LITE.dir/main.cpp.obj"
+  "CMakeFiles/Restauwrong_LITE.dir/main.cpp.obj.d"
+  "Restauwrong_LITE.exe"
+  "Restauwrong_LITE.exe.manifest"
+  "Restauwrong_LITE.pdb"
+  "libRestauwrong_LITE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Restauwrong_LITE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
